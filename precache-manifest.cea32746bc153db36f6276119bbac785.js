ee58e9091dada15959db39b6be1f4e7d27065201
urban89 <urban89@users.noreply.github.com>
2024-03-29 22:48:53 +0000

    tic tac
    tic tac

---
+++ precache-manifest.cea32746bc153db36f6276119bbac785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce1f42defe83bbd08781999bb9129d6",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "775f756b9ca77827ec2c",
+    "url": "/react-tictactoe/static/css/main.eabe9e12.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/react-tictactoe/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/react-tictactoe/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "775f756b9ca77827ec2c",
+    "url": "/react-tictactoe/static/js/main.18ea8bad.chunk.js"
+  },
+  {
+    "revision": "8affd0ef98fc6a9f0b5e",
+    "url": "/react-tictactoe/static/js/runtime~main.f2f031d4.js"
+  },
+  {
+    "revision": "1aba17fbc52fe97f6bf7d7e9da866c2d",
+    "url": "/react-tictactoe/static/media/logo.1aba17fb.svg"
+  }
+]);
